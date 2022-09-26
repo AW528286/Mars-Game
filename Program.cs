704bbb2024280309be13562538e3273484b848cb
Woollard528286 <528286@chrome.escg.ac.uk>
2022-09-26 14:29:34 +0100

    sui

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string Explore = "Explore?";
+        Console.WriteLine($"You have landed on the Vacent Planet Mars. {Explore}");
+        string answer = Console.ReadLine();
+        if (answer == "Yes" | answer == "yes")
+
+        {
+            Console.WriteLine("You chose to explore");
+        }
+
+
+        else
+        {
+            Console.WriteLine("You chose to go back to earth, or die a painful death");
+        }
+        string Inspect = "Inspect?";
+        Console.WriteLine($"after walking for several minutes, you come across a item. {Inspect}");
+        string answer = Console.ReadLine();
+        if (answer == "Yes" | answer == "yes")
+
+        {
+            Console.WriteLine("You chose to inspect the item. It is a piece of Mars Rock. You placed it in your bag and continue to explore");
+        }]
+
+
+        else
+        {
+            Console.WriteLine("you Adventure up a hill where yo find a vast cave bekoning")
+        }
+    }
+      
+}
+
